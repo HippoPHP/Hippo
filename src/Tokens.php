10c680cc7774b719377e5ae9befef4906ece9e42
James Brooks <james@bluebaytravel.co.uk>
2014-10-14 14:20:59 +0100

    Use old Tokens

--- src/Tokens.php
+++ src/Tokens.php
@@ -1,84 +1,48 @@
-<?php 
+<?php
+	
+	define('T_NEW_LINE', 10000);
+	define('T_TAB', 10001);
+	define('T_SEMICOLON', 10002); // ;
+	define('T_BRACES_OPEN', 10003); // { To avoid confusion with T_CURLY_OPEN
+	define('T_BRACES_CLOSE', 10004); // }
+	define('T_PARENTHESIS_OPEN', 10005); // (
+	define('T_PARENTHESIS_CLOSE', 10006); // )
+	define('T_COMMA', 10007); // ,
+	define('T_EQUAL', 10008); // =
+	define('T_CONCAT', 10009); // .
+	define('T_COLON', 10010); // :
+	define('T_MINUS', 10011); // -
+	define('T_PLUS', 10012); // +
+	define('T_IS_GREATER', 10013); // >
+	define('T_IS_SMALLER', 10014); // <
+	define('T_MULTIPLY', 10015); // *
+	define('T_DIVIDE', 10016); // /
+	define('T_QUESTION_MARK', 10017); // ?
+	define('T_MODULO', 10018); // %
+	define('T_EXCLAMATION_MARK', 10019); // !
+	define('T_AMPERSAND', 10020); // %
+	define('T_SQUARE_BRACKET_OPEN', 10021); // [
+	define('T_SQUARE_BRACKET_CLOSE', 10022); // ]
+	define('T_AROBAS', 10023); // @
+	define('T_QUOTE', 10024); // " (only detected before and after a T_ENCAPSED_AND_WHITESPACE) otherwise should be included in T_CONSTANT_ENCAPSED_STRING
+	define('T_UNKNOWN', -1);
 
-	define('T_NONE', 0);
-	define('T_OPEN_CURLY_BRACKET', 1000);
-	define('T_CLOSE_CURLY_BRACKET', 1001);
-	define('T_OPEN_SQUARE_BRACKET', 1002);
-	define('T_CLOSE_SQUARE_BRACKET', 1003);
-	define('T_OPEN_PARENTHESIS', 1004);
-	define('T_CLOSE_PARENTHESIS', 1005);
-	define('T_COLON', 1006);
-	define('T_STRING_CONCAT', 1007);
-	define('T_INLINE_THEN', 1008);
-	define('T_INLINE_ELSE', 1009);
-	define('T_NULL', 1010);
-	define('T_FALSE', 1011);
-	define('T_TRUE', 1012);
-	define('T_SEMICOLON', 1013);
-	define('T_EQUAL', 1014);
-	define('T_MULTIPLY', 1015);
-	define('T_DIVIDE', 1016);
-	define('T_PLUS', 1017);
-	define('T_MINUS', 1018);
-	define('T_MODULUS', 1019);
-	define('T_POWER', 1020);
-	define('T_BITWISE_AND', 1021);
-	define('T_BITWISE_OR', 1022);
-	define('T_ARRAY_HINT', 1023);
-	define('T_GREATER_THAN', 1024);
-	define('T_LESS_THAN', 1025);
-	define('T_BOOLEAN_NOT', 1026);
-	define('T_SELF', 1027);
-	define('T_PARENT', 1028);
-	define('T_DOUBLE_QUOTED_STRING', 1029);
-	define('T_COMMA', 1030);
-	define('T_HEREDOC', 1031);
-	define('T_PROTOTYPE', 1032);
-	define('T_THIS', 1033);
-	define('T_REGULAR_EXPRESSION', 1034);
-	define('T_PROPERTY', 1035);
-	define('T_LABEL', 1036);
-	define('T_OBJECT', 1037);
-	define('T_COLOUR', 1038);
-	define('T_HASH', 1039);
-	define('T_URL', 1040);
-	define('T_STYLE', 1041);
-	define('T_ASPERAND', 1042);
-	define('T_DOLLAR', 1043);
-	define('T_TYPEOF', 1044);
-	define('T_CLOSURE', 1045);
-	define('T_BACKTICK', 1046);
-	define('T_START_NOWDOC', 1047);
-	define('T_NOWDOC', 1048);
-	define('T_END_NOWDOC', 1049);
-	define('T_OPEN_SHORT_ARRAY', 1050);
-	define('T_CLOSE_SHORT_ARRAY', 1051);
-	define('T_GOTO_LABEL', 1052);
-
-	if (!defined('T_NAMESPACE')) {
-		define('T_NAMESPACE', 1053);
-	}
-
-	if (!defined('T_NS_SEPARATOR')) {
-		define('T_NS_SEPARATOR', 1054);
-	}
-
-	if (!defined('T_GOTO')) {
-		define('T_GOTO', 1055);
-	}
-
-	if (!defined('T_TRAIT')) {
-		define('T_TRAIT', 1056);
-	}
-
-	if (!defined('T_INSTEADOF')) {
-		define('T_INSTEADOF', 1057);
-	}
-
-	if (!defined('T_CALLABLE')) {
-		define('T_CALLABLE', 1058);
-	}
-
+	// PHP 5.3 parsing with an older version
 	if (!defined('T_FINALLY')) {
-		define('T_FINALLY', 1059);
+		define('T_FINALLY', 10025);
+	}
+	if (!defined('T_CALLABLE')) {
+		define('T_CALLABLE', 10026);
+	}
+	if (!defined('T_TRAIT')) {
+		define('T_TRAIT', 10027);
+	}
+	if (!defined('T_TRAIT_C')) {
+		define('T_TRAIT_C', 10028);
+	}
+	if (!defined('T_INSTEADOF')) {
+		define('T_INSTEADOF', 10029);
+	}
+	if (!defined('T_YIELD')) {
+		define('T_YIELD', 10030);
 	}
