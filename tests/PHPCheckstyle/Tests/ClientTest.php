9a4ee6bedd7dfddb9252e24100d39885e238e123
James Brooks <james@bluebaytravel.co.uk>
2014-10-15 08:26:14 +0100

    Pass the tests

--- tests/PHPCheckstyle/Tests/ClientTest.php
+++ tests/PHPCheckstyle/Tests/ClientTest.php
@@ -5,5 +5,10 @@
 	use PHPCheckstyle\Client;
 
 	class ClientTest extends \PHPUnit_Framework_TestCase {
-		$this->assertTrue(TRUE);
+		/**
+		 * Please don't hurt me. I'll change this soon.
+		 */
+		public function passTests() {
+			$this->assertTrue(TRUE);
+		}
 	}
