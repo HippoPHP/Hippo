0c562cc7adc43d56ea5d5b642a4f1566f1362eb2
James Brooks <jbrooksuk@me.com>
2014-10-27 22:53:26 +0000

    DocBlock the File class

--- src/File.php
+++ src/File.php
@@ -6,19 +6,53 @@
 	use SeekableIterator;
 	use SplDoublyLinkedList;
 
+	/**
+	 * File class.
+	 */
 	class File extends SplDoublyLinkedList implements SeekableIterator {
+		/**
+		 * Filename.
+		 * @var string
+		 */
 		protected $filename;
 
+		/**
+		 * Source code within the file.
+		 * @var string
+		 */
 		protected $source;
 
+		/**
+		 * Encoding of the file.
+		 * @var string
+		 */
 		protected $encoding;
 
+		/**
+		 * Lines of the file.
+		 * @var array
+		 */
 		protected $lines = array();
 
+		/**
+		 * Violations held against the file.
+		 * @var array
+		 */
 		protected $violations = array();
 
+		/**
+		 * End of line character.
+		 * @var string
+		 */
 		protected $eolChar = '';
 
+		/**
+		 * Create a new file representation.
+		 * @param string $filename
+		 * @param string $source
+		 * @param string $encoding
+		 * @return File
+		 */
 		public function __construct($filename = NULL, $source = NULL, $encoding = 'UTF-8') {
 			$this->filename = $filename;
 			$this->source = NULL;
@@ -32,28 +66,55 @@
 					'ending' => $line['ending']
 				);
 			}
+
+			return $this;
 		}
 
+		/**
+		 * Return the filename.
+		 * @return string
+		 */
 		public function getFilename() {
 			return $this->filename;
 		}
 
+		/**
+		 * Return the source of the file.
+		 * @return string
+		 */
 		public function getSource() {
 			return $this->source;
 		}
 
+		/**
+		 * Return the individual array of lines.
+		 * @return array
+		 */
 		public function getLines() {
 			return $this->lines;
 		}
 
+		/**
+		 * Return the file encoding.
+		 * @return string
+		 */
 		public function getEncoding() {
 			return $this->encoding;
 		}
 
+		/**
+		 * Adds a new Violation to the file.
+		 * @param Violation $violation
+		 */
 		public function addViolation(Violation $violation) {
 			$this->violations[] = $violation;
 		}
 
+		/**
+		 * Return all of the violations on the file.
+		 * Violations are sorted on a line/column basis.
+		 * @return array
+		 */
 		public function getViolations() {
 			usort($this->violations, function(Violation $a, Violation $b) {
 				if ($a->getLine() === $b->getLine()) {
