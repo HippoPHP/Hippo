c060fc1955909056a2c3d6401ac54260e388c97d
James Brooks <james@bluebaytravel.co.uk>
2014-10-29 15:53:46 +0000

    Tokenizer test

--- tests/PHPCheckstyle/Tests/TokenizerTest.php
+++ tests/PHPCheckstyle/Tests/TokenizerTest.php
@@ -5,12 +5,45 @@
 	use PHPCheckstyle\Tokenizer;
 
 	class TokenizerTest extends \PHPUnit_Framework_TestCase {
+		protected $tokenizer;
+
 		public function setUp() {
 			$this->filename = 'test.php';
-			$this->source = '<?php echo 1; ?>';
+
+			$this->tokenizer = new Tokenizer;
 		}
 
-		public function testTrue() {
-			return $this->assertTrue(TRUE);
+		public function testTokenizeWithDefaultNamespace() {
+$source = <<<ESRC
+<?php 
+	namespace PHPCheckstyle;
+	if (0 > 1) {
+		echo 'What? Zero can never be higher.';
+	} else {
+		echo "Cool.";
+	}
+	echo 'Hello'; 
+?>
+ESRC;
+			$this->assertInstanceOf('PHPCheckstyle\File', $this->tokenizer->tokenize($this->filename, $source));
 		}
+
+				public function testTokenizeWithIdentNamespace() {
+$source = <<<ESRC
+<?php 
+	namespace PHPCheckstyle {
+		function foo() {
+			return "bar";
+		}
+	}
+	if (0 > 1) {
+		echo 'What? Zero can never be higher.';
+	} else {
+		echo "Cool.";
+	}
+	echo 'Hello'; 
+?>
+ESRC;
+					$this->assertInstanceOf('PHPCheckstyle\File', $this->tokenizer->tokenize($this->filename, $source));
+				}
 	}
