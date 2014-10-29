5216f6d7b369cc78190132c913fd951738fa7ced
James Brooks <james@bluebaytravel.co.uk>
2014-10-29 08:34:35 +0000

    Added CheckResultTest

---
+++ tests/PHPCheckstyle/Tests/CheckResultTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+	namespace PHPCheckstyle\Tests;
+
+	use PHPCheckstyle;
+	use PHPCheckstyle\File;
+	use PHPCheckstyle\Violation;
+	use PHPCheckstyle\CheckResult;
+
+	class CheckResultTest extends \PHPUnit_Framework_TestCase {
+		protected $instance;
+
+		public function __construct() {
+			$this->instance = new CheckResult;
+		}
+
+		public function testConstructor() {
+			$this->assertInstanceOf('PHPCheckstyle\CheckResult', $this->instance);
+		}
+
+		public function testEmptyByDefault() {
+			$this->assertEmpty($this->instance->getViolations());
+		}
+
+		public function testCount() {
+			$this->assertEquals(0, $this->instance->count());
+		}
+
+		public function testAddViolation() {
+			$file = new File('test.php', '<? echo 1; ?>');
+			$violation = new Violation($file, 1, 1, Violation::SEVERITY_ERROR, 'Do not use short opening tags.', '<?');
+			$this->instance->addViolation($violation);
+
+			$this->assertTrue($this->instance->hasFailed());
+			$this->assertFalse($this->instance->hasSucceeded());
+		}
+
+	}
