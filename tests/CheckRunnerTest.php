4e6ee4d22867aadddd6c21e6e83015497048493e
James Brooks <jbrooksuk@me.com>
2014-11-16 13:44:59 +0000

    Added CheckRunnerTest (all tests are unimplemented)

---
+++ tests/CheckRunnerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+	namespace HippoPHP\Hippo\Tests;
+
+	use \HippoPHP\Hippo;
+	use \HippoPHP\Hippo\CheckRunner;
+
+	class CheckRunnerTest extends \PHPUnit_Framework_TestCase {
+		protected $instance;
+		protected $file;
+
+		public function setUp() {
+			// $this->instance = new CheckResult;
+		}
+
+		public function testSetObserver() {
+			$this->markTestIncomplete(
+				'This test has not been implemented yet.'
+			);
+		}
+
+		public function testCheckPath() {
+			$this->markTestIncomplete(
+				'This test has not been implemented yet.'
+			);
+		}
+
+		public function testCheckFile() {
+			$this->markTestIncomplete(
+				'This test has not been implemented yet.'
+			);
+		}
+	}
