74e489b1f42c54ff7b2a6c4c39804c87a7b97194
James Brooks <jbrooksuk@me.com>
2014-11-16 13:56:02 +0000

    Start working on the test

--- tests/CheckRunnerTest.php
+++ tests/CheckRunnerTest.php
@@ -2,15 +2,22 @@
 
 	namespace HippoPHP\Hippo\Tests;
 
-	use \HippoPHP\Hippo;
+	use \HippoPHP\Hippo\Config;
+	use \HippoPHP\Hippo\FileSystem;
 	use \HippoPHP\Hippo\CheckRunner;
+	use \HippoPHP\Hippo\CheckRepository;
+	use \HippoPHP\Hippo\Config\YAMLConfigReader;
 
 	class CheckRunnerTest extends \PHPUnit_Framework_TestCase {
 		protected $instance;
-		protected $file;
 
 		public function setUp() {
-			// $this->instance = new CheckResult;
+			$fileSystem = new FileSystem;
+			$checkRepository = new CheckRepository($fileSystem);
+			$configReader = new YAMLConfigReader($fileSystem);
+			// $config = $configReader->loadFromFile
+
+			// $this->instance = new CheckRunner($fileSystem, $checkRepository, $configReader);
 		}
 
 		public function testSetObserver() {
