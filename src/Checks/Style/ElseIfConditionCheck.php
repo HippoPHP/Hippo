c2a9d61bf099ac58c56e5aea39245aef4c9db8c0
James Brooks <jbrooksuk@me.com>
2014-11-16 18:47:49 +0000

    Checks that we use elseif over else if

---
+++ src/Checks/Style/ElseIfConditionCheck.php
@@ -0,0 +1,57 @@
+<?php
+
+	namespace HippoPHP\Hippo\Checks\Naming;
+
+	use \HippoPHP\Hippo\CheckContext;
+	use \HippoPHP\Hippo\Config\Config;
+	use \HippoPHP\Hippo\Checks\AbstractCheck;
+	use \HippoPHP\Hippo\Checks\CheckInterface;
+	use \HippoPHP\Hippo\Exception\BadConfigKeyException;
+
+	class ElseIfConditionCheck extends AbstractCheck implements CheckInterface {
+		/**
+		 * @return string
+		 */
+		public function getConfigRoot() {
+			return 'style.elseif';
+		}
+
+		/**
+		 * checkFileInternal(): defined by AbstractCheck.
+		 * @see AbstractCheck::checkFileInternal()
+		 * @param CheckContext $checkContext
+		 * @param Config $config
+		 * @return void
+		 */
+		protected function checkFileInternal(CheckContext $checkContext, Config $config) {
+			$file = $checkContext->getFile();
+			$tokens = $checkContext->getTokenList();
+
+			try {
+				do {
+					// Jump us to the next token we want to check.
+					$tokens->seekToType(T_ELSE);
+
+					// Move to the next token.
+					$tokens->next();
+					$token = $tokens->current();
+
+					// Check that the next token is not equal to T_WHITESPACE T_IF.
+					if ($token->isType(T_WHITESPACE)) {
+						$tokens->next(); // Move forward.
+						$nextToken = $tokens->current();
+						if ($nextToken->isType(T_IF)) {
+							$this->addViolation(
+								$file,
+								$token->getLine(),
+								$token->getColumn(),
+								'Use `elseif` rather than `else if`'
+							);
+						}
+					}
+				} while ($tokens->valid());
+			} catch (\HippoPHP\Tokenizer\Exception\OutOfBoundsException $e) {
+				// Ignore the exception, we're at the end of the file.
+			}
+		}
+	}
